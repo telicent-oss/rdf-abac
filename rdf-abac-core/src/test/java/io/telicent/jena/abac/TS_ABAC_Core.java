2b24a2f0f5a827ba1a413888f3705a52555b2e9c
Rob Walpole <183595007+robwtelicent@users.noreply.github.com>
2024-11-21 19:44:15 +0000

    Adding TestAttributeValueSet

--- rdf-abac-core/src/test/java/io/telicent/jena/abac/TS_ABAC_Core.java
+++ rdf-abac-core/src/test/java/io/telicent/jena/abac/TS_ABAC_Core.java
@@ -32,6 +32,7 @@ import org.junit.platform.suite.api.Suite;
     , TestHierarchy.class
 
     , TestAttributeValue.class
+    , TestAttributeValueSet.class
     , TestAttributeValueList.class
 
     , TestAttributeExprEval.class
--- rdf-abac-core/src/test/java/io/telicent/jena/abac/TestAttributeValueSet.java
+++ rdf-abac-core/src/test/java/io/telicent/jena/abac/TestAttributeValueSet.java
@@ -1,11 +1,31 @@
 package io.telicent.jena.abac;
 
 import io.telicent.jena.abac.attributes.Attribute;
+import io.telicent.jena.abac.attributes.AttributeValue;
+import io.telicent.jena.abac.attributes.ValueTerm;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AttributeValueSetTest {
+public class TestAttributeValueSet {
+
+    @Test
+    public void testOfTrue() {
+        AttributeValue value = AttributeValue.of("some", ValueTerm.value(true));
+        AttributeValueSet set = AttributeValueSet.of(value);
+        Attribute attr = new Attribute("some");
+        assertTrue(set.hasAttribute(attr));
+    }
+
+    @Test
+    public void testOfFalse() {
+        AttributeValue value = AttributeValue.of("some", ValueTerm.value(true));
+        AttributeValueSet set = AttributeValueSet.of(value);
+        Attribute attr = new Attribute("other");
+        assertFalse(set.hasAttribute(attr));
+    }
 
     @Test
     public void testHasAttributeTrue() {
@@ -24,27 +44,68 @@ public class AttributeValueSetTest {
     @Test
     public void testAsString() {
         AttributeValueSet set = AttributeValueSet.of("some");
-        assertEquals(set.asString(),"some=true");
+        assertEquals("some=true", set.asString());
     }
 
     @Test
     public void testEqualsTrue() {
         AttributeValueSet set1 = AttributeValueSet.of("test");
         AttributeValueSet set2 = AttributeValueSet.of("test");
-        assertEquals(set1, set2);
+        assertTrue(set1.equals(set2)); // we are specifically testing the equals method here
+    }
+
+    @Test
+    public void testEqualsTrueAsSame() {
+        AttributeValueSet set1 = AttributeValueSet.of("test");
+        AttributeValueSet set2 = AttributeValueSet.of("test");
+        assertTrue(set1.equals(set1)); // we are specifically testing the equals method here
     }
 
     @Test
     public void testEqualsFalse() {
         AttributeValueSet set1 = AttributeValueSet.of("some");
         AttributeValueSet set2 = AttributeValueSet.of("other");
-        assertNotEquals(set1, set2);
+        assertFalse(set1.equals(set2)); // we are specifically testing the equals method here
+    }
+
+    @Test
+    public void testEqualsFalseAsNull() {
+        AttributeValueSet set1 = AttributeValueSet.of("some");
+        assertFalse(set1.equals(null)); // we are specifically testing the equals method here
+    }
+
+    @Test
+    public void testEqualsFalseAsDifferentClass() {
+        AttributeValueSet set1 = AttributeValueSet.of("test");
+        String test = "test";
+        assertFalse(set1.equals(test)); // we are specifically testing the equals method here
     }
 
     @Test
     public void testHashCode() {
         AttributeValueSet set = AttributeValueSet.of("test");
         assertEquals(3595359,set.hashCode());
+    }
+
+    @Test
+    public void testIsEmptyTrue() {
+        AttributeValueSet set = AttributeValueSet.of();
+        assertTrue(set.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyFalse() {
+        AttributeValueSet set = AttributeValueSet.of("test");
+        assertFalse(set.isEmpty());
+    }
+
+    @Test
+    public void testAttributes() {
+        AttributeValueSet set = AttributeValueSet.of("some","other");
+        Collection<Attribute> attrs = set.attributes();
+        assertEquals(2, attrs.size());
+        assertTrue(attrs.contains(new Attribute("some")));
+        assertTrue(attrs.contains(new Attribute("other")));
     }
 
 }
