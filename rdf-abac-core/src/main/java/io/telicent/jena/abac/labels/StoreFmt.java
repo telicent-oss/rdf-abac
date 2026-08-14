88c2f8b36326c2fee8d70623954b1e6eff4be1af
Paul Gallagher <paul.gallagher@telicent.io>
2026-08-14 14:57:41 +0100

    [CORE-1458] Addressing SonarQube issues - Replace this assert with a proper check

--- rdf-abac-core/src/main/java/io/telicent/jena/abac/labels/StoreFmt.java
+++ rdf-abac-core/src/main/java/io/telicent/jena/abac/labels/StoreFmt.java
@@ -23,6 +23,12 @@ import java.util.List;
 @SuppressWarnings({ "java:S115", "java:S112", "java:S6355", "java:S1133", "java:S1186", "java:S135" })
 public interface StoreFmt {
 
+    private static void requireLittleEndian(ByteBuffer byteBuffer) {
+        if (byteBuffer.order() != ByteOrder.LITTLE_ENDIAN) {
+            throw new IllegalArgumentException("ByteBuffer must use little-endian byte order");
+        }
+    }
+
     /**
      * The ordering of {@code NodeType} enum values was initially defined for {@code Any} to be last in order to respect
      * how wildcards are processed in seeking {@link LegacyLabelsStoreRocksDB}. As the ordinal values will be encoded on
@@ -198,8 +204,7 @@ public interface StoreFmt {
      * @param i          the integer
      */
     static void encodeInt(ByteBuffer byteBuffer, int i) {
-
-        assert byteBuffer.order() == ByteOrder.LITTLE_ENDIAN;
+        requireLittleEndian(byteBuffer);
         byteBuffer.putInt(i);
     }
 
@@ -283,7 +288,7 @@ public interface StoreFmt {
      * @return the integer value retrieved from the bytebuffer
      */
     static int decodeInt(ByteBuffer byteBuffer) {
-        assert byteBuffer.order() == ByteOrder.LITTLE_ENDIAN;
+        requireLittleEndian(byteBuffer);
         return byteBuffer.getInt();
     }
 
@@ -300,8 +305,7 @@ public interface StoreFmt {
      * @return how much space the long value takes up.
      */
     static IntBytes formatLongVariable(final ByteBuffer byteBuffer, long value) {
-
-        assert byteBuffer.order() == ByteOrder.LITTLE_ENDIAN;
+        requireLittleEndian(byteBuffer);
         if (value > Integer.MAX_VALUE || value < Integer.MIN_VALUE) {
             byteBuffer.putLong(value);
             return IntBytes.EightBytes;
@@ -325,8 +329,7 @@ public interface StoreFmt {
      * @return the decoding of the encoded long
      */
     static long parseLongVariable(final ByteBuffer byteBuffer, IntBytes size) {
-
-        assert byteBuffer.order() == ByteOrder.LITTLE_ENDIAN;
+        requireLittleEndian(byteBuffer);
         return switch (size) {
             case OneByte -> byteBuffer.get();
             case TwoBytes -> byteBuffer.getShort();
